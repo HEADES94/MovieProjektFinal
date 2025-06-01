6729e316f43811309fdc969bc3be032dafdfdb70
joshui <joshuapaoletti@googlemail.com>
2025-06-02 00:05:08 +0200

    Add initial project structure with AI movie recommendation features

---
+++ movie_api.py
@@ -0,0 +1,25 @@
+import dotenv
+import os
+import requests
+
+
+dotenv.load_dotenv()
+
+API_KEY = os.getenv("OMDB_API_KEY")
+
+BASE_URL = "http://www.omdbapi.com/?i=tt3896198&apikey=" + API_KEY
+
+class OMDBClient:
+    def __init__(self):
+        self.BASE_URL = BASE_URL
+
+    def get_movie(self, title: str) -> dict | None:
+        url = self.BASE_URL + "&t=" + title
+        response = requests.get(url)
+        if response.status_code == 200 and response.json()["Response"] == "True":
+            new_movie = {"name": response.json()["Title"], "director": response.json()["Director"],
+                         "year": response.json()["Year"], "poster": response.json()["Poster"],
+                         "country": response.json()["Country"], "genre": response.json()["Genre"],
+                         "plot": response.json()["Plot"], "rating": response.json()["imdbRating"]}
+            return new_movie
+        return None
